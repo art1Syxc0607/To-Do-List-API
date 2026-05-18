1c6a50e6b904dff36104665ed6e0e9d626f7bb89
art1Syxc0607 <salart357@gmail.com>
2026-05-18 10:50:50 +0700

    бращ

--- DataAccess/Entities/Category.cs
+++ DataAccess/Entities/Category.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace DataAccess.Entities;
 
-namespace DataAccess.Entities
+public class Category
 {
-    public class Category
-    {
-    }
 }
+
--- DataAccess/Entities/Tag.cs
+++ DataAccess/Entities/Tag.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace DataAccess.Entities;
 
-namespace DataAccess.Entities
+public class Tag
 {
-    public class Tag
-    {
-    }
+
 }
+
